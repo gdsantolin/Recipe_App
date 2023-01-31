46e2485a34555d9e474233339c6457ff9141fbd1
Gustavo D <gdsantolin@gmail.com>
2023-01-30 23:08:03 -0300

    parte inicial

---
+++ app/src/main/java/com/example/recipe_app/database/RecipeDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.recipe_app.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.recipe_app.dao.RecipeDao
+import com.example.recipe_app.entities.Recipes
+
+@Database(entities = [Recipes::class], version = 1, exportSchema = false)
+abstract class RecipeDatabase: RoomDatabase() {
+    companion object{
+        var recipesDatabase:RecipeDatabase? = null
+
+        @Synchronized
+        fun getDatabase(context: Context): RecipeDatabase{
+            if(recipesDatabase != null){
+                recipesDatabase = Room.databaseBuilder(
+                    context,
+                    RecipeDatabase::class.java,
+                    "recipe.db"
+                ).build()
+            }
+            return recipesDatabase!!
+        }
+    }
+
+    abstract fun recipeDao(): RecipeDao
+}
